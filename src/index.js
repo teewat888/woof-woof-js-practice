41bd602295779e7a0a308f0a37234f0d6dd56648
teerawat prarom <teewat@yahoo.com>
2021-08-14 12:20:18 +1000

    add toggle good dog bad dog

--- src/index.js
+++ src/index.js
@@ -1,18 +1,73 @@
 document.addEventListener("DOMContentLoaded", () => {
   const dogBar = document.querySelector("div#dog-bar");
+  const dogInfo = document.querySelector("div#dog-info");
+
   const baseURL = "http://localhost:3000/pups";
-  const clearTags = () => {};
+  const clearTags = () => {
+    while (dogInfo.firstChild) {
+        dogInfo.removeChild(dogInfo.firstChild);
+      }
+  };
+  const setAttributes = (el, attrs) => {
+    for (let key in attrs) {
+      el.setAttribute(key, attrs[key]);
+    }
+  };
+  const goodOrbad = (isGoodDog) => {
+    if (isGoodDog) {
+      return "Good Dog!";
+    } else {
+      return "Bad Dog!";
+    }
+  };
 
   const createDogBar = (data) => {
-   // console.log("data passed to createdDogBar: ", data);
+    // console.log("data passed to createdDogBar: ", data);
     for (const item of data) {
       const spanB = document.createElement("span");
+      const img = document.createElement("img");
+      const h2 = document.createElement("h2");
+
+      const button = document.createElement("button");
       spanB.innerText = item.name;
-      spanB.addEventListenerI('click',() => {
-          //put and display doog content here
-      })
+
+      spanB.addEventListener("click", () => {
+        //put and display doog content here
+        clearTags();
+        console.log("fire span event");
+        setAttributes(img, {
+          src: `${item.image}`,
+          width: "300",
+          height: "300",
+        });
+        h2.innerText = item.name;
+
+        button.innerText = goodOrbad(item.isGoodDog);
+
+        dogInfo.append(img, h2, button);
+      });
       //console.log("dogBar: ", dogBar);
       dogBar.appendChild(spanB);
+      button.addEventListener("click", () => {
+        item.isGoodDog = !item.isGoodDog;
+        confObj = {
+          method: "PATCH",
+          header: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify({
+            isGoodDog: item.isGoodDog,
+          }),
+        };
+        fetch(baseURL + "/" + item.id, confObj)
+          .then((resp) => resp.json())
+          .then((data) => {
+            button.innerText = goodOrbad(item.isGoodDog);
+            console.log(data);
+          })
+          .catch((e) => console.log(e));
+      });
     }
   };
 
@@ -20,7 +75,6 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(baseURL)
       .then((resp) => resp.json())
       .then((data) => {
-        //console.log("data: ", data);
         createDogBar(data);
       })
       .catch((e) => {
